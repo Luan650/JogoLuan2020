7da3ab7d4f21fdd9ac3ce630cc5d2bea0b31f86c
Luan Pimenta <luancpimenta@gmail.com>
2020-03-12 10:29:29 -0300

    Using events

--- Assets/Scripts/NavGame/Core/CombatGameObject.cs
+++ Assets/Scripts/NavGame/Core/CombatGameObject.cs
@@ -9,6 +9,8 @@ namespace NavGame.Core
     {
         float cooldown = 0f;
 
+        public OnAttackHitEvent onAttackHit;
+
         protected virtual void Update()
         {
             DecreaseAttackCooldown();
@@ -19,7 +21,10 @@ namespace NavGame.Core
             {
                 cooldown = 1f / stats.attackSpeed;
                 target.TakeDamage(stats.damage);
-                AudioManager.instance.Play("enemy-hit", target.transform.position);
+                if(onAttackHit != null)
+                {
+                    onAttackHit(target.transform.position);
+                }
             }
         }
 
---
+++ Assets/Scripts/NavGame/Core/Events.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace NavGame.Core
+{
+    // CHARACTER EVENTS
+    public delegate void OnAttackHitEvent(Vector3 position);
+}
+

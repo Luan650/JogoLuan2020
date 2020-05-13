e1ce6b54f63c654efb66a1c7b440c6d6512033f1
Luan Pimenta <luancpimenta@gmail.com>
2020-05-13 15:58:40 -0300

    Highlight action

--- Assets/Scripts/NavGame/Managers/LevelManager.cs
+++ Assets/Scripts/NavGame/Managers/LevelManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NavGame.Core;
 
 namespace NavGame.Managers
 {
@@ -9,6 +10,10 @@ namespace NavGame.Managers
     {
         public static LevelManager instance;
         public Action[] actions;
+
+        public OnActionSelectEvent onActionSelect;
+
+        public OnActionCancelEvent onActionCancel;
 
         protected int selectedAction = -1;
 
@@ -31,21 +36,35 @@ namespace NavGame.Managers
 
         public virtual void SelectedAction(int actionIndex)
         {
-            Debug.Log("Selected: " + actions[actionIndex].prefab.name);
+            CancelAction();
             selectedAction = actionIndex;
+            if (onActionSelect != null)
+            {
+                onActionSelect(actionIndex);
+            }
         }
 
         public virtual void DoAction(Vector3 point)
         {
-            Debug.Log("Do: " + actions[selectedAction].prefab.name);
             Instantiate(actions[selectedAction].prefab, point, Quaternion.identity);  
+            int index = selectedAction;
+            selectedAction = -1;
+            if (onActionCancel != null)
+            {
+                onActionCancel(index);
+            }
         }
 
         public virtual void CancelAction()
         {
             if (selectedAction != -1)
             {
+                int index = selectedAction;
                 selectedAction = -1;
+                if(onActionCancel != null)
+                {
+                    onActionCancel(index);
+                }
             } 
         }
 

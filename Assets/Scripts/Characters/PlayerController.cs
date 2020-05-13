82c5c59fc0b301d2ad108c3f580460fb028f3776
Luan Pimenta <luancpimenta@gmail.com>
2020-05-13 14:56:36 -0300

    Performing action

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using NavGame.Core;
+using NavGame.Managers;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : TouchableGameObject
@@ -37,6 +38,9 @@ public class PlayerController : TouchableGameObject
     {
         if (Input.GetMouseButtonDown(1))
         {
+            LevelManager.instance.CancelAction();
+            actionPoint = Vector3.zero;
+
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
@@ -57,7 +61,7 @@ public class PlayerController : TouchableGameObject
                 pickupTarget = null;
             }
         }
-        else if (Input.GetMouseButtonDown(0))
+        else if (Input.GetMouseButtonDown(0) && LevelManager.instance.IsActionSelected())
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -67,6 +71,19 @@ public class PlayerController : TouchableGameObject
                 actionPoint = hit.point;
                 agent.SetDestination(hit.point);
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            LevelManager.instance.SelectedAction(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            LevelManager.instance.SelectedAction(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            LevelManager.instance.SelectedAction(2);
         }
     }
 
@@ -88,6 +105,7 @@ public class PlayerController : TouchableGameObject
             if (Vector3.Distance(transform.position, actionPoint) <= range)
             {
                 agent.ResetPath();
+                LevelManager.instance.DoAction(actionPoint);
                 actionPoint = Vector3.zero;
             }
         }
